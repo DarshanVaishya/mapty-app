f57d1334974ec5a94cbc29f8704e8a87df6fcd69
DarshanVaishya <DarshanVaishya3@gmail.com>
2022-01-31 15:07:10 +0530

    Refactored code into a class

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,73 +13,89 @@ const inputElevation = document.querySelector(".form__input--elevation");
 const cadenceRow = inputCadence.closest(".form__row");
 const elevationRow = inputElevation.closest(".form__row");
 
-let map;
-let mapEvent;
+class App {
+	#map;
+	#mapEvent;
 
-function createPin(coords, map, className) {
-	// eslint-disable-next-line no-undef
-	const options = L.popup({
-		maxWidth: 250,
-		minWidth: 100,
-		autoClose: false,
-		closeOnClick: false,
-		className,
-	});
+	constructor() {
+		this._getPosition();
 
-	// eslint-disable-next-line no-undef
-	L.marker(coords)
-		.addTo(map)
-		.bindPopup(options)
-		.setPopupContent("Workout")
-		.openPopup();
-}
+		inputType.addEventListener("change", this._toggleElevationField);
 
-function loadMap(position) {
-	const { latitude, longitude } = position.coords;
+		form.addEventListener("submit", this._newWorkout.bind(this));
+	}
 
-	// 13 is zoom level
-	// eslint-disable-next-line no-undef
-	map = L.map("map").setView([latitude, longitude], 14);
+	_getPosition() {
+		if (navigator.geolocation) {
+			// learn/lecture/22649229#questions/16473574
+			navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+				alert("Error encountered while fetching your location.")
+			);
+		}
+	}
 
-	// eslint-disable-next-line no-undef
-	L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-		attribution:
-			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	}).addTo(map);
+	_loadMap(position) {
+		const { latitude, longitude } = position.coords;
 
-	map.on("click", (event) => {
-		mapEvent = event;
+		// 13 is zoom level
+		// eslint-disable-next-line no-undef
+		this.#map = L.map("map").setView([latitude, longitude], 14);
+
+		// eslint-disable-next-line no-undef
+		L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(this.#map);
+
+		this.#map.on("click", this._showForm.bind(this));
+	}
+
+	_showForm(event) {
+		this.#mapEvent = event;
 		form.classList.remove("hidden");
 		inputDistance.focus();
-	});
+	}
+
+	_toggleElevationField() {
+		cadenceRow.classList.toggle("form__row--hidden");
+		elevationRow.classList.toggle("form__row--hidden");
+	}
+
+	_newWorkout(e) {
+		e.preventDefault();
+
+		// Display data
+		const { lat, lng } = this.#mapEvent.latlng;
+		const type = inputType.value;
+		app._createPin([lat, lng], `${type}-popup`);
+
+		// Reseting the inputs
+		inputDistance.value = "";
+		inputDuration.value = "";
+		inputCadence.value = "";
+		inputElevation.value = "";
+
+		// Hide the form
+		form.classList.add("hidden");
+	}
+
+	_createPin(coords, className) {
+		// eslint-disable-next-line no-undef
+		const options = L.popup({
+			maxWidth: 250,
+			minWidth: 100,
+			autoClose: false,
+			closeOnClick: false,
+			className,
+		});
+
+		// eslint-disable-next-line no-undef
+		L.marker(coords)
+			.addTo(this.#map)
+			.bindPopup(options)
+			.setPopupContent("Workout")
+			.openPopup();
+	}
 }
 
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(loadMap, () =>
-		alert("Error encountered while fetching your location.")
-	);
-}
-
-inputType.addEventListener("change", () => {
-	cadenceRow.classList.toggle("form__row--hidden");
-	elevationRow.classList.toggle("form__row--hidden");
-});
-
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-
-	// Display data
-	const { lat, lng } = mapEvent.latlng;
-	const type = inputType.value;
-	createPin([lat, lng], map, `${type}-popup`);
-
-	// Reseting the inputs
-	inputType.value = "running";
-	inputDistance.value = "";
-	inputDuration.value = "";
-	inputCadence.value = "";
-	inputElevation.value = "";
-
-	// Hide the form
-	form.classList.add("hidden");
-});
+const app = new App();
